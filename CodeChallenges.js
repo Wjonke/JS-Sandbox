717bb27e3e7f06cb69e9a25112778fb8148ae161
Wes Jonke <wesjonke@gmail.com>
2019-06-13 11:34:03 -0400

    completed several code challenges on Edebit  and added solutions here

--- CodeChallenges.js
+++ CodeChallenges.js
@@ -4,8 +4,9 @@
 function nextEdge(side1, side2) {
 	return (side1 + side2) - 1
 }
-//-------------------------------------------------------------------------
 
+// Is the Number Even or Odd?
+// Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
 function isEvenOrOdd(num) {
 	if (num%2 === 0) {
 		return 'even';
@@ -13,3 +14,37 @@ function isEvenOrOdd(num) {
 		return 'odd';
 	}
 }
+
+// Is the Number Less than or Equal to Zero?
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+function lessThanOrEqualToZero(num) {
+	if(num <= 0){
+    return true;
+  } else{
+		return false;
+	}
+}
+
+// Char-to-ASCII
+// Create a function that returns the ASCII value of the passed in character.
+
+function ctoa(c) {
+	return c.charCodeAt(0);
+}
+
+// Check String for Spaces
+// Create a function that returns true if a string contains any spaces.
+
+//const hasSpaces = str => str.includes(" ");
+	function hasSpaces(str) {
+    return str.includes(' ')
+  }
+
+
+  // Is the Word Singular or Plural?
+  // Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
+
+  function isPlural(word) {
+    return word.endsWith("s");
+  }
